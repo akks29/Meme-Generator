1c4e85e9f91dea26e452af61553f6dfbe546287e
Akshaya A.R <94378042+akks29@users.noreply.github.com>
2023-05-13 17:29:36 +0530

    2nd commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="./troll-face.png" className="header--image" />
+      <h2 className="header--title"> MEME GENERATOR </h2>{" "}
+     
+    </header>
+  );
+}
